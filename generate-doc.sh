aa242590fb6855f90f7fcae2ee15d5f55bef7e15
Mehdi ABAAKOUK <sileht@sileht.net>
2025-10-22 09:03:23 +0200

    ci: fix auto doc jobs
    This change:
    * upgrades all GitHub actions version
    * regenerates the README.md to match 3.6.0 format
    
    Change-Id: I453f0e44e46af712a3b05e230bb37e1ea7d431de

--- generate-doc.sh
+++ generate-doc.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 command -v auto-doc >/dev/null 2>&1 || { echo "auto-doc is not installed" >&2; exit 1; }
 
-auto-doc -f action.yml
+auto-doc -f action.yml --useCodeBlocks --repository Mergifyio/gha-mergify-ci --colMaxWords 120 --useMajorVersion
